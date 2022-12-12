1c421d553f009a84c373c7efe8ca82b7202c22ae
jakob_jenko <jakob.jenko@xlab.si>
2022-12-12 14:05:50 +0100

    Override to point to local plugin

---
+++ bundler.d/local_plugins.rb
@@ -0,0 +1,11 @@
+#override_gem 'manageiq-providers-cisco_intersight', :path => File.expand_path('../plugins/manageiq-providers-cisco_intersight', __dir__)
+
+#override_gem 'manageiq-providers-amazon', :path => File.expand_path('../plugins/manageiq-providers-amazon',__dir__)
+
+override_gem "manageiq-providers-minio", :path => "../plugins/manageiq-providers-minio"
+
+#override_gem "manageiq-providers-autosde", :path => "../plugins/manageiq-providers-autosde"
+
+#gem 'pry'
+#gem 'pry-remote'
+#gem 'pry-nav'
